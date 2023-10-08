e08e9542b9b1a6ddb959c094f7f65460500d6311
zhangqiang <1023547026@qq.com>
2023-10-08 16:53:52 +0800

    更新Vue

--- Vue3/005_Composition API/compositionapi/src/003_响应式API/App.vue
+++ Vue3/005_Composition API/compositionapi/src/003_响应式API/App.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
 		<h4>{{ message }}</h4>
+
 		<h4>当前计数：{{ counter }}</h4>
 		<button @click="increment">+1</button>
 
@@ -20,9 +21,12 @@
 			let message = 'Hello world';
 
 			// 定义响应式数据
-			// ref函数：主要用于定义普通数据为响应式数据（也可以定义复杂类型的数据）
-			// reactive函数：主要用于定义复杂类型的数据为响应式数据
-			// 所以，在定义响应式数据的时候，可以都使用ref函数，并且ref对象是可更改的，当我们为ref对象的value属性赋予新的值时，它也是响应式的。
+			/**
+			 * ref函数：
+			 * 接收一个内部值（基本类型/复杂类型），返回一个响应式的、可更改的ref对象，此对象只有一个指向其内部值的属性.value。
+			 * 如果给ref传入一个复杂类型的值，那么其内部将通过reactive()将其转为具有深层次响应式的对象。
+			 */
+			// reactive函数：用于对复杂类型（对象/数组）进行深层次的响应式转换，其所有的属性包含嵌套的属性都将具有响应式。
 			let counter = ref(0);
 			function increment() {
 				counter.value++;
@@ -38,7 +42,7 @@
 				age: 20
 			});
 
-			// 为ref对象的value属性赋予新的值时，它也是响应式的
+			// 为ref对象的value属性赋予新的值时，它也是响应式的（比如直接赋值一个新对象）
 			function changeInfo() {
 				info1.value = {
 					name: '李四1',
