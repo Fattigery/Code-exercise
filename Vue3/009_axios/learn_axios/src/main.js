27f4e3e9b273007edfd38f67b0c49278f4fe1efb
zhangqiang <1023547026@qq.com>
2023-11-09 10:18:21 +0800

    更新

---
+++ Vue3/009_axios/learn_axios/src/main.js
@@ -0,0 +1,33 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import axios from 'axios';
+
+// 1.发送request请求
+axios
+	.request({
+		url: 'http://123.207.32.32:8000/home/multidata'
+	})
+	.then(res => {
+		console.log(res.data);
+	});
+
+// 2.发送get请求
+axios.get('http://123.207.32.32:8000/home/multidata').then(res => {
+	console.log(res.data);
+});
+
+// 3.发送post请求
+axios
+	.post('http://123.207.32.32:1888/02_param/postjson', {
+		// request body（请求体）
+		data: {
+			name: 'coderwhy',
+			password: 123456
+		}
+	})
+	.then(res => {
+		console.log(res.data);
+	});
+
+createApp(App).mount('#app');
